29e332fd1bfe5e7c71fd54c390cd7af17b3a3b18
Orson Peters <orsonpeters@gmail.com>
2013-02-05 17:09:05 +0100

    work on paper

--- node/kbucket.py
+++
@@ -1,9 +0,0 @@
-
-
-class KBucket(object):
-    def __init__(self, k, dht):
-        self.k = k
-        self.dht = dht
-
-    def add_node(self, ip, port, node_id):
-        
---
+++ paper/make.bat
@@ -0,0 +1,5 @@
+@taskkill /IM sumatraPDF.exe
+@latex paper
+@bibtex paper
+@pdflatex paper
+@start paper.pdf
